9ebcc7d30949b6ebc9298fd259e4d65466ff11f3
trotskoa <72283393+trotskoa@users.noreply.github.com>
2024-05-08 16:31:14 +0200

    image+colors

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,10 @@
 export default {
     primary: '#006992',
     secondary: '#ECA400',
+    accent_green: '#EAF8BF',
+    titles: '#27476E',
+    text: '#202244',
     black: '#000',
     white: '#fff'
+
 }
