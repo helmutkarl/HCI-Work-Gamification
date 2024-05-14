7e3bdfa83cbbe70728fc14b97d9c628872faf45c
ukahharov <umidkahharov@gmail.com>
2024-05-14 21:14:14 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,26 +2,46 @@ import React from 'react';
 import AppNavigator from './app/components/Navigation.js';
 import { AuthProvider } from './app/components/AuthContext.js';
 import { MenuProvider } from 'react-native-popup-menu';
-import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 
-
-
-export default App = () => {
-  const [fontsLoaded] = useFonts({
-    'Montserrat-Bold': require('./assets/fonts/Montserrat/Montserrat-Bold.ttf'),
-    'Montserrat-Regular': require('./assets/fonts/Montserrat/Montserrat-Regular.ttf'),
-    'Montserrat-Semibold': require('./assets/fonts/Montserrat/Montserrat-Semibold.ttf'),
-    'Montserrat-Light': require('./assets/fonts/Montserrat/Montserrat-Light.ttf'),
-    'OpenSans-Bold': require('./assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
-    'OpenSans-Regular': require('./assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
-    'OpenSans-Semibold': require('./assets/fonts/Open_Sans/OpenSans-Semibold.ttf'),
-    'OpenSans-Light': require('./assets/fonts/Open_Sans/OpenSans-Light.ttf'),
-  });
-  return (
-    <AuthProvider>
-      <MenuProvider>
-        <AppNavigator />
-      </MenuProvider>
-    </AuthProvider>
-  );
+let customFonts = {
+    'Montserrat-Bold': require('./app/assets/fonts/Montserrat/Montserrat-Bold.ttf'),
+    'Montserrat-Regular': require('./app/assets/fonts/Montserrat/Montserrat-Regular.ttf'),
+    'Montserrat-Semibold': require('./app/assets/fonts/Montserrat/Montserrat-SemiBold.ttf'),
+    'Montserrat-Light': require('./app/assets/fonts/Montserrat/Montserrat-Light.ttf'),
+    'OpenSans-Bold': require('./app/assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
+    'OpenSans-Regular': require('./app/assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
+    'OpenSans-Semibold': require('./app/assets/fonts/Open_Sans/OpenSans-SemiBold.ttf'),
+    'OpenSans-Light': require('./app/assets/fonts/Open_Sans/OpenSans-Light.ttf'),
 };
+
+
+
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return null;
+    }
+
+    return (
+      <AuthProvider>
+        <MenuProvider>
+          <AppNavigator />
+        </MenuProvider>
+      </AuthProvider>
+    );
+  }
+}
